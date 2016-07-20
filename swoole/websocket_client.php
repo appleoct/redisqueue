be9d8d2c06ecc66342617cb6aaa112baad27955f
lize <459985175@qq.com>
2016-07-20 15:14:29 +0800

    聊天添加表情 Signed-off-by: lize <459985175@qq.com>

--- swoole/websocket_client.php
+++ swoole/websocket_client.php
@@ -13,6 +13,7 @@ if(!isset($_SESSION['nickname']))
     <meta charset="UTF-8">
     <title>首页</title>
     <script type="text/javascript" src="jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="jquery.qqFace.js"></script>
     <style>
         *{margin:0px; padding:0px;}
         #content{width:80%; margin:0px auto;}
@@ -23,6 +24,8 @@ if(!isset($_SESSION['nickname']))
     <div id="content">
          <h1 style="margin:20px auto;text-align:center;">
             <?php echo title;?> welcome ! <?php echo $_SESSION['nickname'];?>
+            
+           
         </h1>
         
         <div style="border:1px solid red;height:300px; overflow:scroll; margin-bottom:20px;">
@@ -31,22 +34,23 @@ if(!isset($_SESSION['nickname']))
             </table>
         </div>
         
-        <form>
+      
             发送聊天内容:
-            <textarea rows="5" cols="100" id="content_message"></textarea>
-         <input type="button" id="send_message" value="发送" />
-        </form>
+            <div id="content_message" style="width:500px;height:200px; border:1px solid red;" contenteditable="true">
+                
+            </div>
+            
+             <div class="emotion">表情</div>
+            <input type="button" id="send_message" value="发送" />
+     
     </div>
 
-   
     
     <script>
     
     //websocket
     var exampleSocket = new WebSocket("ws://192.168.33.10:9501");
 	  exampleSocket.onopen = function (event) {
-/* 		  var default_message = '';
-		  exampleSocket.send(default_message); */
 	  };
 
 
@@ -61,25 +65,39 @@ if(!isset($_SESSION['nickname']))
 
 		//打开页面直接发送一条信息
 		
+
+		  $(function(){
+	 	    	$('.emotion').qqFace({
+		    		id : 'facebox', //表情盒子的ID
+		    		assign:'content_message', //给那个控件赋值
+		    		path:'face/'	//表情存放的路径
+		    	}); 
+
+		    	$('#send_message').click(function(){
+
+		            var content_message = $('#content_message').html();
+
+		            if(content_message == "")
+		            {
+		          	  alert('内容不能为空');
+		                return false;   			
+		            }else
+		            {
+		                var content = nickname + "," + content_message;
+		            	
+		      		  exampleSocket.send(content); 
+		      		  
+		      		  $('#content_message').html(''); 		 
+		      		  	 
+		            }
+		          });
+		    	
+		    });
+		 
+		 
+	
 	    
-    	$('#send_message').click(function(){
-
-          var content_message = $('#content_message').val();
-
-          if(content_message == "")
-          {
-        	  alert('内容不能为空');
-              return false;   			
-          }else
-          {
-              var content = nickname + "," + content_message;
-          	
-    		  exampleSocket.send(content); 
-    		  
-    		  $('#content_message').val(''); 		 
-    		  	 
-          }
-        });
+    	
     	
     </script>
 </body>
